1245a2eccdb8a14945b79321005b947bc97bf199
Quan <21013237@st.phenikaa-uni.edu.vn>
2023-08-25 17:18:23 +0700

    a

---
+++ connect.php
@@ -0,0 +1,15 @@
+<?php 
+    // Kết nối đến cơ sở dữ liệu
+    $servername = "localhost:3306";
+    $username = "root";
+    $password = "";
+    $dbname = "shareimage";
+
+    $connection = new mysqli($servername, $username, $password, $dbname);
+
+    // Kiểm tra kết nối
+    if ($connection->connect_error) {
+        die("Không thể kết nối đến cơ sở dữ liệu: " . $conn->connect_error);
+    }
+    echo "Kết nối thành công!";
+?>
