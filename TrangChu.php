73f13d82613d967713cd89778e62def509a0aeb1
Vũ Ngọc Văn <vungocvan.zio7.03@gmail.com>
2023-09-06 02:59:23 +0700

    update frontend Home Page

--- TrangChu.php
+++ TrangChu.php
@@ -16,39 +16,121 @@
 <body>
     
     <header>
-        <div class="container">
-            <div class="start" style="padding-left: 15px;">
-                <button class="toolbar" style="border: none; background-color: white;">
-                    <span class="material-symbols-outlined">menu</span>
+    <div class="container">
+            <div class="start" style="height: 45px;">
+                <button class="toolbar" style="position: relative;width: 45px;height: 45px; border: none; background-color: transparent;">
+                    <span style="display: block; font-size: 35px;align-items: center;" class="material-symbols-outlined">menu</span>
                 </button>
                 <a href="Trangchu.php" class="logo">
                     <img src="images/logo.png" alt="" width="25%">
                 </a>
             </div>
-            <div class="center" style="padding-left: 25%; padding-right: 25%; padding-top: 25px; border-radius: 15px;">
+            <div class="center" style="padding-left: 25%; padding-right: 25%; padding-top: 15px; border-radius: 15px;">
                 <form action="Search.php" method="post" style="display: flex;">
-                    <input type="search" name="noidung" autocomplete="off" placeholder="Nhập nội dung tìm kiếm" style="width: 550px;border-radius: 15px; outline: none;">
-                    <button class="search-button" type="submit" name="btn" style="border-radius: 15px;background-color: white;">
+                    <input type="search" name="noidung" autocomplete="off" placeholder="Nhập nội dung tìm kiếm" style="width: 550px;border-radius: 15px; outline: none;padding-left: 15px;">
+                    <button class="search-button" type="submit" name="btn" style="width: 40px;border-radius: 15px;background-color: white;">
                         <span class="material-symbols-outlined">search</span>
                     </button>
                 </form>
             </div>
-            <div class="end">
-                <button class="avatar" style="height: 40px; width: 40px; border: none; background-color: white; padding-left: 25px;">
-                    <img src="" alt="Hình ảnh đại diện" style="border-radius: 25px;">
+            <div class="end" style="height: 45px;">
+                <button class="avatar" style="height: 45px; width: 45px; border: none; border-radius: 50%;">
+                    <img src="./images/Minion-Crazy-icon.jpg" alt="Hình ảnh đại diện" style="border-radius: 25px;">
                 </button>
             </div>
         </div>
         
     </header>
+
     <div class="webContent">
         <div class="leftMenu">
-            <a href=""><p>Trang chủ</p></a>
-            <a href=""><p>Album</p></a>
-            <a href=""><p>Ảnh yêu thích</p></a>
+            <div class="btn home">
+                <a href="">
+                    <span class="material-symbols-outlined">house</span>
+                    <span class="title">Trang chủ</span>
+                </a>
+            </div>
+            <div class="btn album">
+                <a href="">
+                    <span class="material-symbols-outlined">photo_album</span>
+                    <span class="title">Album</span>
+                </a>
+            </div>
+            <div class="btn favourite">
+                <a href="">
+                    <span class="material-symbols-outlined">favorite</span>
+                    <span class="title">Ảnh yêu thích</span>
+                </a>
+            </div>
+            <div class="btn home">
+                <a href="">
+                    <span class="material-symbols-outlined">house</span>
+                    <span class="title">Trang chủ</span>
+                </a>
+            </div>
+            <div class="btn album">
+                <a href="">
+                    <span class="material-symbols-outlined">photo_album</span>
+                    <span class="title">Album</span>
+                </a>
+            </div>
+            <div class="btn favourite">
+                <a href="">
+                    <span class="material-symbols-outlined">favorite</span>
+                    <span class="title">Ảnh yêu thích</span>
+                </a>
+            </div>
+            <div class="btn home">
+                <a href="">
+                    <span class="material-symbols-outlined">house</span>
+                    <span class="title">Trang chủ</span>
+                </a>
+            </div>
+            <div class="btn album">
+                <a href="">
+                    <span class="material-symbols-outlined">photo_album</span>
+                    <span class="title">Album</span>
+                </a>
+            </div>
+            <div class="btn favourite">
+                <a href="">
+                    <span class="material-symbols-outlined">favorite</span>
+                    <span class="title">Ảnh yêu thích</span>
+                </a>
+            </div>
+            <div class="btn home">
+                <a href="">
+                    <span class="material-symbols-outlined">house</span>
+                    <span class="title">Trang chủ</span>
+                </a>
+            </div>
+            <div class="btn album">
+                <a href="">
+                    <span class="material-symbols-outlined">photo_album</span>
+                    <span class="title">Album</span>
+                </a>
+            </div>
+            <div class="btn favourite">
+                <a href="">
+                    <span class="material-symbols-outlined">favorite</span>
+                    <span class="title">Ảnh yêu thích</span>
+                </a>
+            </div>
+            
+            
         </div>
-        <div class="mainContainer">
-            <a href=""><img src="" alt="Ảnh"></a>
+        
+        <div class="mainContainer" style="width: 100%;display: block;">
+            <div class="content" >
+
+                <a class="mainContainer__img " href=""><div class="img--border"><img class="image" src="./images/Minion-Crazy-icon.jpg" alt="Ảnh"></div></a>
+                <a class="mainContainer__img2" href=""><div class="img--border"><img class="image" src="./images/images (1).jfif" alt="Ảnh"></div></a>
+                <a class="mainContainer__img3" href=""><div class="img--border"><img class="image" src="./images/image2 .jfif" alt="Ảnh"></div></a>
+                <a class="mainContainer__img4" href=""><div class="img--border"><img class="image" src="./images/Minion-Crazy-icon.jpg" alt="Ảnh"></div></a>
+                <a class="mainContainer__img5" href=""><div class="img--border"><img class="image" src="./images/images (1).jfif" alt="Ảnh"></div></a>
+                <a class="mainContainer__img6" href=""><div class="img--border"><img class="image" src="./images/image2 .jfif" alt="Ảnh"></div></a>
+            
+            </div>
         </div>
     </div>
 </body>
